4c3a88540ca0e7a40976f47b87b9929d81c01da0
omar morales <omarmorales865@gmail.com>
2013-10-08 13:55:30 -0400

    primer comit
    Jugando con java

---
+++ holamundo.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class holamundo{
+	public static void main(String[] args) {
+		int x = 1500;// Esta es la variavle X igual sepudiera llamar patito y funcionaria
+		//for (int hola=0; 1<patito; hola++)
+        System.out.println("No debo llegar tarde ");
+        for(int i = 0; i < x; i++)/* aqui estamos definiedo la variavle "i" dandole el valor de cero
+        "i" se refiere a index se pudiera llamar patito2 y funcinaria
+        en la parte "i < x " le estamos diciendo que mintras "i" se menor que "x" le agrege una hasta llegar a
+        a 1500. por que a 1500? porque es el valor que le dimos a "x"
+
+        */
+            System.out.println("No debo llegar tarde "+ i);
+		
+	}
+}
+         //for(int x = 10; x < 20; x = x+1)
